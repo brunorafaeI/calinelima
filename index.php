a49572f8bfbc97b8044bb9f5ff8eeed1745c9183
Bruno Rafael <brunorafaelap@gmail.com>
2017-01-23 14:28:23 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Caline Lima - Portofolio</title>
+</head>
+<body>
+	
+	<header>
+	<section>
+		<h1>caline lima</h1>
+		<p>photography</p>		
+	</section>
+		<nav>
+			<a href="javascript:void(0)">Home</a>
+			<a href="javascript:void(0)">About</a>
+			<a href="javascript:void(0)">Works</a>
+			<a href="javascript:void(0)">Contact</a>		
+		</nav>		
+	</header>
+<main>
+	<section>
+		
+		
+	</section>
+	<article>
+			
+	</article>
+	<aside>
+			
+	</aside>
+</main>
+	<footer>
+		<details>
+		  <summary>Copyright 2017 - <?= date("Y") ?>.</summary>
+		  <p> - by Bruno Rafael. All Rights Reserved.</p>
+		  <p>All content and graphics on this web site are the property of the owner Bruno Rafael.</p>
+		</details>
+	</footer>
+
+</body>
+</html>
